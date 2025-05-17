475acb9fdb4d0ebbc8a07792c81e783a42aba2f9
Henry Arnold <devharnold@gmail.com>
2025-05-17 23:01:58 +0300

    Code: Finally handled a String problem

--- MergeStringsAlternately/mergeStringsAtlernately.py
+++ MergeStringsAlternately/mergeStringsAtlernately.py
@@ -2,4 +2,25 @@
 
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        pass
+        i = 0
+        j = 0
+        result = ""
+
+        while i < len(word1) or j < len(word2):
+            if i < len(word1):
+                result += word1[i]
+                i += 1
+            if j < len(word2):
+                result += word2[j]
+                j += 1
+
+        return result
+
+# Explanation
+"""
+First set 2 different pointers, 
+one will point of the first character of word1 and the other will point to the last character of word2
+Then while on both word1 and word2,
+Take both characters alternately between word1 and word2 until both strings are done
+Then return the merged string.
+"""
---
+++ RomantoInteger/romanToInt.py
@@ -0,0 +1,5 @@
+# 13 - Roman to Integer
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        pass
