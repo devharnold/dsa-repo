775c3dfb8ea739a5a5b409167812dd27696917b3
Henry Arnold <devharnold@gmail.com>
2025-07-21 03:57:43 +0300

    Roman to Integer Solution

--- RomantoInteger/romanToInt.py
+++ RomantoInteger/romanToInt.py
@@ -2,7 +2,23 @@
 
 class Solution:
     def romanToInt(self, s: str) -> int:
-        pass
+        roman_map = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        sum = 0
+        i = 0
 
+        """
+        Using this example:
+        Input s = "MCMXCIV"
+        M  C  M  X  C  I  V
 
-    I, V, X, L, C, D, M
+        Output = 1994"""
+
+        while i < len(s):
+            if i + 1 < len(s) and roman_map[s[i]] < roman_map[s[i + 1]]:
+                sum += roman_map[s[i + 1]] - roman_map[s[i]]
+                i += 2
+            else:
+                sum += roman_map[s[i]]
+                i += 1
+
+        return sum
