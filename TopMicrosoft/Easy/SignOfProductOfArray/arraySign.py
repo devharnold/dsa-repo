6f4ef946881afe6598b415c319517e699921b226
Henry Arnold <devharnold@gmail.com>
2025-07-27 18:05:26 +0300

    Fix: comment approach to solution

--- TopMicrosoft/Easy/SignOfProductOfArray/arraySign.py
+++ TopMicrosoft/Easy/SignOfProductOfArray/arraySign.py
@@ -4,6 +4,10 @@ from typing import List
 
 class Solution:
     def arraySign(self, nums: List[int]) -> int:
+        # first initialize result to 1
+        # then if n = 0, then the product will automatically be 0
+        # if n < 0: this means that n is negative, multiply the result by -1
+        # return the result
         result = 1
         for n in nums:
             if n == 0:
